00fc4bd19d24a67712dc2983bbb5ef17fb9b551b
Pecherskiy88 <pecherskiy88@gmail.com>
2019-04-22 21:39:42 +0300

    add scroll

--- src/components/Sidebar/data.js
+++ src/components/Sidebar/data.js
@@ -11,6 +11,11 @@ const data = [
     description: 'This is description TWO',
     tasks: [{ title: 'Task two 1' }, { title: 'Task two 2' }],
   },
+  {
+    title: 'Goal THIRD SUPER',
+    description: 'This is description TWO',
+    tasks: [{ title: 'Task third 1' }, { title: 'Task third 2' }],
+  },
 ];
 
 export default data;
