33bf1841b2b9b964a02dad0d22a360faea568bd0
iRoy <44869601+monolithphoenix@users.noreply.github.com>
2019-04-23 04:34:22 +0300

    Mobile view ready PROTO

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,8 @@ import SVG from 'react-inlinesvg';
 
 import './HeaderMobile.css';
 import './HeaderTablet.css';
+import './HeaderDesctop.css';
+
 import { ReactComponent as More } from '../../assets/images/icons/more/baseline-more_vert-24px.svg';
 import { ReactComponent as Dashboard } from '../../assets/images/icons/dashboard/baseline-dashboard-24px.svg';
 import { ReactComponent as Results } from '../../assets/images/icons/chart/baseline-timeline-24px.svg';
@@ -12,19 +14,17 @@ const Header = () => {
   return (
     <header className="header">
       <div className="header__icons">
-        <a href="localhost/dots" className="header__router">
-          <More className="header__icon more" />
-        </a>
-        <a href="localhost/dashboard" className="header__router">
+        <More className="header__icon more" />
+        <a href="localhost/dashboard" className="header__router dashboard">
           <Dashboard className="header__icon dashboard" />
           <span>Dashboard</span>
         </a>
-        <a href="localhost/results" className="header__router">
+        <p className="header__date">Date</p>
+        <a href="localhost/results" className="header__router results">
           <Results className="header__icon results" />
           <span>Results</span>
         </a>
       </div>
-      <p className="header__date">Date</p>
       <p className="header__logo">Sculptor</p>
       <a href="localhost/logout" className="header__router logout">
         Logout
