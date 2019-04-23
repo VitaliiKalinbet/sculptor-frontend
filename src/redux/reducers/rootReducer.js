bb72da6eb54207652f11e60455232d97a1539a30
ashappp <you@example.com>
2019-04-23 12:24:23 +0300

    add datePickerReducer and datePickerAction

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,9 +1,11 @@
 /*eslint-disable*/
 import { combineReducers } from 'redux';
 import showReducer from './sidebarReducer';
+import activeDateReducer from './datePickerReducer';
 
 const rootReducer = combineReducers({
   showReducer,
+  activeDateReducer,
 });
 
 export default rootReducer;
