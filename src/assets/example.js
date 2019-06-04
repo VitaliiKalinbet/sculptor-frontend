50f39d61f01838dc557d0d3e4f79a64290cbbc6d
VitaliiKalinbet <bosssvit@gmail.com>
2019-06-04 14:06:54 +0300

    change dev

--- src/assets/example.js
+++ src/assets/example.js
@@ -31,34 +31,51 @@ const arr2 = [
   },
 ];
 
-const newGoalArr = [];
-const oldGoalArr = [];
+const onlyOldTasks = [];
+const onlyNewTasks = [...arr2];
 
-for (el of arr2) {
-  for (item of arr1) {
-    if (el.id === item.id) {
-      oldGoalArr.push(el);
+for (let oldTask of arr1) {
+  onlyNewTasks.map((task, index) => {
+    if (task.id === oldTask.id) {
+      const splicedElement = onlyNewTasks.splice(index, 1);
+      console.log('splicedElement', splicedElement);
+      onlyOldTasks.push(splicedElement[0]);
     }
-
-    if (el.id !== item.id) {
-      if (newGoalArr.includes(el)) {
-        console.log('el.id :', el.id);
-        console.log('item.id :', item.id);
-        console.log('includes');
-        break;
-      } else {
-        console.log('el.id :', el.id);
-        console.log('item.id :', item.id);
-        console.log('not includes');
-        newGoalArr.push(el);
-      }
-    } else {
-      break;
-    }
-  }
+    return onlyNewTasks;
+  });
 }
-console.log('newGoalArr :', newGoalArr);
-console.log('oldGoalArr :', oldGoalArr);
+
+console.log('onlyOldTasks: ', onlyOldTasks);
+console.log('onlyNewTasks: ', onlyNewTasks);
+
+// const newGoalArr = [];
+// const oldGoalArr = [];
+
+// for (el of arr2) {
+//   for (item of arr1) {
+//     if (el.id === item.id) {
+//       oldGoalArr.push(el);
+//     }
+
+//     if (el.id !== item.id) {
+//       if (newGoalArr.includes(el)) {
+//         console.log('el.id :', el.id);
+//         console.log('item.id :', item.id);
+//         console.log('includes');
+//         break;
+//       } else {
+//         console.log('el.id :', el.id);
+//         console.log('item.id :', item.id);
+//         console.log('not includes');
+//         newGoalArr.push(el);
+//       }
+//     } else {
+//       break;
+//     }
+//   }
+// }
+// console.log('newGoalArr :', newGoalArr);
+// console.log('oldGoalArr :', oldGoalArr);
 
 // const oldGoalArr = arr1.map(el => arr2.find(obj => obj.id === el.id));
 // console.log('oldGoalArr :', oldGoalArr);
