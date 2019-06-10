113aefc0b9e05ffad78f444307a4e144ce2397dd
VitaliiKalinbet <bosssvit@gmail.com>
2019-06-10 16:03:21 +0300

    done fix_shedule

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,91 +1,69 @@
+/*eslint-disable*/
 import React, { Component } from 'react';
 import { Line } from 'react-chartjs-2';
-import './Statistics.css';
+import { connect } from 'react-redux';
+import moment from 'moment';
+import styles from './Statistics.module.css';
 
 class Statistics extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      weekRange: [],
-      dateArr: [],
-      isDone: [],
-      incomplete: [],
-      dateNow: new Date(new Date(2019, 3, 19).setHours(0, 0, 0, 0)),
+      doneTasksArr: this.props.weekTasks.arrDays.map(
+        el => el.tasks.filter(item => item.isDone).length,
+      ),
+      plannedTasksArr: this.props.weekTasks.arrDays.map(el => el.tasks.length),
+      weekDate: this.props.weekTasks.arrDays.map(el =>
+        moment(el.data).format('DD-MM'),
+      ),
+      dateNow: new Date(this.props.weekTasks.date).toISOString(),
     };
   }
 
   componentDidMount() {
-    const token =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNWNiOTk2M2QwNmI5NjFhMTAyNWQ2MDAwIiwiZW1haWwiOiJnb29nbGVAZ28uY29tIiwiaWF0IjoxNTU2MDg2NjgzfQ.bzMkR4T8--Ri_mn9XWtsvRnG14OtDu77nwjsH91D8ZA';
-
-    const url = 'http://192.168.90.200:8000/api/goal/5cb9963d06b961a1025d6000';
-    fetch(url, { headers: { Authorization: `Bearer ${token}` } })
-      .then(res => res.json())
-      .then(data => data.data)
-      .then(data => this.mapper(data));
+    this.setState({
+      doneTasksArr: this.props.weekTasks.arrDays.map(
+        el => el.tasks.filter(item => item.isDone).length,
+      ),
+      plannedTasksArr: this.props.weekTasks.arrDays.map(el => el.tasks.length),
+      weekDate: this.props.weekTasks.arrDays.map(el =>
+        moment(el.data).format('DD-MM'),
+      ),
+    });
   }
 
-  mapper = data => {
-    const date = [];
-    const newArr = [];
-    data.map(el => newArr.push(...el.goalTasks));
-    newArr.map(el => date.push(...el.taskActiveDates));
-    this.filterByDone(date);
-    this.setState({
-      weekRange: [...newArr],
-      dateArr: [...date],
-    });
-  };
-
-  filterByDone = arr => {
-    const isDone = [];
-    const incomplete = [];
-    arr.forEach(el => (el.isDone ? isDone.push(el) : incomplete.push(el)));
-    this.setState({
-      isDone,
-      incomplete,
-    });
-  };
-
-  getDay = (date, count = 0) => {
-    return new Date(
-      ((date && new Date(date)) || new Date()).setHours(0, 0, 0, 0) +
-        count * 3600 * 24 * 1000,
-    ).getTime();
-  };
-
-  filtredDataByDay = arr => {
-    const { dateNow } = this.state;
-    const arrDate = [0, 0, 0, 0, 0, 0, 0];
-
-    if (arr.length) {
-      arr.map(el => {
-        if (this.getDay(el.date) === this.getDay(dateNow)) {
-          arrDate[0] += 1;
-        } else if (this.getDay(el.date) === this.getDay(dateNow, 1)) {
-          arrDate[1] += 1;
-        } else if (this.getDay(el.date) === this.getDay(dateNow, 2)) {
-          arrDate[2] += 1;
-        } else if (this.getDay(el.date) === this.getDay(dateNow, 3)) {
-          arrDate[3] += 1;
-        } else if (this.getDay(el.date) === this.getDay(dateNow, 4)) {
-          arrDate[4] += 1;
-        } else if (this.getDay(el.date) === this.getDay(dateNow, 5)) {
-          arrDate[5] += 1;
-        } else if (this.getDay(el.date) === this.getDay(dateNow, 6)) {
-          arrDate[6] += 1;
-        } else if (this.getDay(el.date) === this.getDay(dateNow, 7)) {
-          arrDate[7] += 1;
-        }
-        return null;
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.weekTasks.date !== this.props.weekTasks.date) {
+      this.setState({
+        dateNow: new Date(this.props.weekTasks.date).toISOString(),
       });
     }
-
-    return arrDate;
-  };
+    if (prevProps.weekTasks.arrDays !== this.props.weekTasks.arrDays) {
+      this.setState({
+        doneTasksArr: this.props.weekTasks.arrDays.map(
+          el => el.tasks.filter(item => item.isDone).length,
+        ),
+        plannedTasksArr: this.props.weekTasks.arrDays.map(
+          el => el.tasks.length,
+        ),
+        weekDate: this.props.weekTasks.arrDays.map(el =>
+          moment(el.data).format('DD.MM.YY'),
+        ),
+      });
+    }
+  }
 
   render() {
-    const { weekRange, dateArr, isDone, incomplete, dateNow } = this.state;
+    const {
+      weekRange,
+      dateArr,
+      isDone,
+      incomplete,
+      dateNow,
+      doneTasksArr,
+      plannedTasksArr,
+      weekDate,
+    } = this.state;
 
     const options = {
       ticks: {
@@ -125,11 +103,11 @@ class Statistics extends Component {
       dateArr,
       isDone,
       incomplete,
-      labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+      labels: weekDate || ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
       datasets: [
         {
           label: 'Planned',
-          data: this.filtredDataByDay(weekRange),
+          data: plannedTasksArr,
           backgroundColor: ['rgba(40,64,96)'],
           fill: 'none',
           borderColor: ['rgba(40,64,96,1)'],
@@ -140,7 +118,7 @@ class Statistics extends Component {
         },
         {
           label: 'Done',
-          data: this.filtredDataByDay(isDone),
+          data: doneTasksArr,
           backgroundColor: ['rgba(252,132,44)'],
           borderColor: ['rgba(252,132,44,1)'],
           pointRadius: '3',
@@ -158,18 +136,31 @@ class Statistics extends Component {
     };
 
     return (
-      <div className="Statistics">
-        <h1>HELLO</h1>
-        <div className="Statick_text_wrapper">
-          <span className="Statick_circle circle_planned" />
-          <p className="Statistics_label planned">Planned</p>
-          <span className="Statick_circle circle_done" />
-          <p className="Statistics_label done">Done</p>
+      <div className={styles.Statistics}>
+        <div className={styles.Statick_text_wrapper}>
+          <span
+            className={`${styles.Statick_circle} ${styles.circle_planned}`}
+          />
+          <p className={`${styles.Statistics_label} ${styles.planned}`}>
+            Planned
+          </p>
+          <span className={`${styles.Statick_circle} ${styles.circle_done}`} />
+          <p className={`${styles.Statistics_label} ${styles.done}`}>Done</p>
         </div>
-        <Line data={data} options={options} className="line_chart" />
+        <Line data={data} options={options} className={styles.line_chart} />
       </div>
     );
   }
 }
 
-export default Statistics;
+function mapStateToProps(state) {
+  return {
+    weekTasks: state.weekTasks,
+    goals: state.goals,
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  null,
+)(Statistics);
